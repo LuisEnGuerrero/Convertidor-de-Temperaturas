ec91b6b256d4905e33c4eb8d7f306773ca2fc8e8
Luis Enrique Guerrero <luisenguerrero@yahoo.com>
2021-04-28 22:58:49 -0500

    Edición Beta V.0.2

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+            <footer className="footer">
+              <div className="color-red padding-10 text-align-center font-color-white">
+                <a.link href='mailto: luisenguerrero@yahoo.com'></a.link>
+                <h4>
+                      luisenguerrero@yahoo.com
+                </h4>
+              </div>
+            </footer>  );
+    }
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+export default function Profile() {
+    return (
+          <nav className="main">
+            <div className="color-brown padding-10">
+                <h1>Convertidor de Temperaturas.</h1>
+            </div>
+          </nav>
+      );
+    }
+    
