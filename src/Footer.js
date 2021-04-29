5a69a7ee8d1dbb76972a1f8efa3a54e9951a18ae
Luis Enrique Guerrero <luisenguerrero@yahoo.com>
2021-04-28 23:11:17 -0500

    Fix: mail - Edición Beta V.0.2.3

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,9 @@ export default function Footer() {
     return (
             <footer className="footer">
               <div className="color-red padding-10 text-align-center font-color-white">
-                <a.link href='mailto: luisenguerrero@yahoo.com'></a.link>
+                <a rel="luisguerrero@misena.edu.co" href="mailto:luisguerrero@misena.edu.co" target="_blank"><img alt="luisguerrero@misena.edu.co"/></a>
                 <h4>
-                      luisenguerrero@yahoo.com
+                    luisguerrero@misena.edu.co
                 </h4>
               </div>
             </footer>  );
